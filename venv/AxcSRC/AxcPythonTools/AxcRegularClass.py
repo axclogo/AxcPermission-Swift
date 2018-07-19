6836a8c71acb00909f15acc0f9ec9fca9544ede8
AxcLogo <axclogo@163.com>
2018-07-19 15:23:03 +0800

    首推字符串处理、数组字典处理等

---
+++ venv/AxcSRC/AxcPythonTools/AxcRegularClass.py
@@ -0,0 +1,13 @@
+
+
+# class AxcRegularClass
+
+    # def function():
+    #     print('This is a function')
+    #     a = 1 + 2
+    #     print(a)
+
+def function():
+    print('This is a function')
+    a = 1 + 2
+    print(a)
